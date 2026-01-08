f57f1547bd642dff3d2b3dcc897baccb5cde1fb2
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2026-01-08 16:05:03 +0000

    chore(setup):  setup unfinalized, copied other project & repurposing, commiting to save progress

---
+++ src/lib/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Input } from './input/components/input.svelte';
+export { default as RadioHorizontal } from './radio_horizontal/components/RadioHorizontal.svelte';
+export { default as LightDark } from './darklight/Speedial.svelte';
+export { default as Textarea } from './textarea/components/Textarea.svelte';
+export { default as Navigation } from './navigation/Navigation.svelte';
+export { default as Navigation2 } from './navigation/Navigation2.svelte';
+export { default as Refresh } from './refresh/Refresh.svelte';
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+
+  // Global CSS Import
+  import '$lib/css/app.css';
+
+	import { page } from '$app/state';
+	import { onMount } from 'svelte';
+	import { LightDark } from '$lib/components';
+	import { global_mode$ } from '$lib/components/darklight/mode';
+	import Speedial from '$lib/components/speedial/speedial.svelte';
+
+	let pageName = page.url.pathname;
+
+	let mode$ = global_mode$.mode$;
+
+	onMount(() => {
+		mode$.subscribe((v) => {
+			localStorage.theme = v;
+			document.documentElement.classList.toggle('dark', v === 'dark');
+		});
+	});
+</script>
+
+
+{#if pageName === "/register" || pageName === "/login" || pageName === "/account"}
+  <div class="w-full -z-10000 bg-[#F0F0F2] dark:bg-[#1F1F39]">
+    <main class="size-full">
+      <slot />
+      <Speedial />
+    </main>
+  </div>
+{:else if pageName === "/dashboard"}
+  <div class="w-full -z-10000 bg-[#3D5CFF]">
+    <main class="size-full">
+      <slot />
+      <Speedial />
+    </main>
+  </div>
+{:else if pageName.startsWith("/create")}
+  <div class="w-full -z-10000 bg-[#4c4c61] dark:bg-[#1F1F39]">
+    <main class="size-full">
+      <slot />
+      <Speedial />
+    </main>
+  </div>
+{/if}
+
+
+<style>
+	main {
+		max-width: 30rem;
+		margin-inline: auto;
+	}
+</style>
+
+
+<div>
+
+    
+</div>

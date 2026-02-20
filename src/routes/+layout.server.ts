aa3e1c0c6f56174bfb086f6c474c1c358b2e1dbb
dhanani-git-trial <dhanani.githubtrial@gmail.com>
2026-02-20 02:20:48 +0000

    minor()

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,4 +5,9 @@ import { connect_to_db } from "$lib/server/db";
 export const load: LayoutServerLoad = async (event) => {
 	const connection = await connect_to_db();
 	if (!connection) throw error(500, "Database connection failed");
+
+	event.cookies.set('theme.color', '#3d5cff', { 
+    path: '/', 
+    httpOnly: false
+  });
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	// CSS
 	import '../app.css';
 
+
 	import { Flex } from 'sk-clib';
 	import { ThemeInit } from 'sk-clib/theme';
 	
@@ -9,7 +10,7 @@
 
 </script>
 
-<ThemeInit options={{ defaultColor: '#3d5cff', defaultMode: 'dark', defaultVariant: 'vibrant' } as any} />
+<ThemeInit defaults={{ defaultSeedColor: '#3d5cff', defaultMode: 'dark', defaultVariant: 'vibrant' }} />
 
 <Flex fill class="bg-surface overflow-y-auto">
 	<Flex col class="animate mx-auto w-full max-w-full sm:max-w-3/4 md:max-w-3/5 lg:max-w-3/4">
